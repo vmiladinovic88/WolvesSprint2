990bae033616a23f41d0782f32de8ec7ed0be27f
vmiladinovic <veci_m@hotmail.com>
2023-07-17 09:14:51 -0400

    Sharing project

---
+++ src/test/java/com/wolves_sprint_2/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.wolves_sprint_2.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/wolves_sprint_2/step_definitions"
+)
+public class FailedTestRunner {
+}
+
+
