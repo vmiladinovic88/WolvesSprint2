1bb508c7324ded6f914e9b1d3f064741c5ae0837
vmiladinovic <veci_m@hotmail.com>
2023-07-18 09:39:52 -0400

    updated import statement in login page

--- src/test/java/com/wolves_sprint_2/pages/LoginPage.java
+++ src/test/java/com/wolves_sprint_2/pages/LoginPage.java
@@ -1,7 +1,7 @@
 package com.wolves_sprint_2.pages;
 
 
-import com.fleet.utilities.Driver;
+import com.wolves_sprint_2.utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
