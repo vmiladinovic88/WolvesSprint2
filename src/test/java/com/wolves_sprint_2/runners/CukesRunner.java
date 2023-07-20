7af498de4b6726ae84c36b5043bd4846389e8f3d
Elif_Cydeo <elif.demirbass10@gmail.com>
2023-07-19 22:19:38 -0400

    added cukes runner

--- src/test/java/com/wolves_sprint_2/runners/CukesRunner.java
+++ src/test/java/com/wolves_sprint_2/runners/CukesRunner.java
@@ -9,12 +9,13 @@ import org.junit.runner.RunWith;
         plugin = {
                 "html:target/cucumber-reports.html",
                 "rerun:target/rerun.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                "json:target/cucumber.json"
         },
         features = "src/test/resources/features",
         glue = "com/wolves_sprint_2/step_definitions",
-        dryRun = true,
-        tags = " @wip",
+        dryRun = false,
+        tags = "@B29G15-263",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
